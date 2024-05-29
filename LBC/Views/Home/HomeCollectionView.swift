72388676f771c66016124b59915c711e5f5b7f7a
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-29 15:46:50 +0200

    Adds a dynamic cell layout depending on device width

--- LBC/Views/Home/HomeCollectionView.swift
+++ LBC/Views/Home/HomeCollectionView.swift
@@ -17,17 +17,25 @@ final class HomeCollectionView: UICollectionView {
     
     weak var viewModel: HomeViewModel?
     
+    private var interItemSpacing = 16.0
+    
     // MARK: - Life Cycle
     
     init(viewModel: HomeViewModel? = nil) {
         self.viewModel = viewModel
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 200.0, height: 400.0)
-        layout.minimumInteritemSpacing = 0.0
-        layout.minimumLineSpacing = 0.0
+        layout.minimumInteritemSpacing = interItemSpacing
+        layout.minimumLineSpacing = interItemSpacing
         super.init(frame: .zero, collectionViewLayout: layout)
         
         setupCollectionView()
+        observeDeviceOrientation()
+    }
+    
+    // This should not be done in the view. Move later if enough time.
+    private func observeDeviceOrientation() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didChangeDeviceOrientation), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
     
     required init?(coder: NSCoder) {
@@ -36,8 +44,16 @@ final class HomeCollectionView: UICollectionView {
     
     private func setupCollectionView() {
         
+        delegate = self
         dataSource = self
         register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func didChangeDeviceOrientation() {
+        
+        collectionViewLayout.invalidateLayout()
     }
 
 }
@@ -60,6 +76,35 @@ extension HomeCollectionView: UICollectionViewDataSource {
         }
         
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension HomeCollectionView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let availableWidth = collectionView.frame.width
+        let numberOfColumns: Int
+        
+        // Setup an arbitrary dynamic width depending on device width.
+        switch availableWidth {
+        case 0..<500:
+            numberOfColumns = 2
+        case 500..<750:
+            numberOfColumns = 3
+        case 750..<1000:
+            numberOfColumns = 4
+        default:
+            numberOfColumns = 5
+        }
+        
+        let totalInterItemSpacing = ((numberOfColumns - 1) * Int(interItemSpacing))
+        
+        let cellWidth = (availableWidth - CGFloat(totalInterItemSpacing)) / CGFloat(numberOfColumns)
+        
+        return CGSize(width: cellWidth, height: cellWidth * 4/3)
     }
 }
 
