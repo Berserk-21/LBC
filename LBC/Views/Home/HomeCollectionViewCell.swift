f58bd7b6f8ed37704c44b4e861ae3ba74d19afe6
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-29 11:13:18 +0200

    Adds ImageView and Labels to HomeCollectionViewCell

--- LBC/Views/Home/HomeCollectionViewCell.swift
+++ LBC/Views/Home/HomeCollectionViewCell.swift
@@ -13,4 +13,61 @@ final class HomeCollectionViewCell: UICollectionViewCell {
     
     static let identifier: String = "HomeCollectionViewCell"
     
+    private var thumbImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "photo")
+        iv.tintColor = .black
+        return iv
+    }()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16.0)
+        return label
+    }()
+    
+    private var priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16.0, weight: .heavy)
+        return label
+    }()
+
+    private var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0, weight: .light)
+        return label
+    }()
+    
+    private var creationDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 140, weight: .light)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    
+    func setupConstraints() {
+        
+//        let stackView = UIStackView(arrangedSubviews: [])
+    }
+    
+    func configure(model: HomeViewModel) {
+        
+        titleLabel.text = "Dummy title"
+        priceLabel.text = "5000€"
+        categoryLabel.text = "Vehicule"
+        creationDateLabel.text = "10 septembre 2021"
+        
+        
+    }
 }
