63863919cabfa33b31f40d0ec5b5be0f4fdb89f6
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-29 10:49:28 +0200

    Initialize and bind viewModel

--- LBC/Views/Home/HomeCollectionView.swift
+++ LBC/Views/Home/HomeCollectionView.swift
@@ -7,7 +7,30 @@
 
 import UIKit
 
+protocol HomeCollectionViewInterface: AnyObject {
+    
+}
+
 final class HomeCollectionView: UICollectionView {
+    
+    // MARK: - Properties
+    
+    weak var viewModel: HomeViewModel?
+    
+    // MARK: - Life Cycle
+    
+    init(viewModel: HomeViewModel? = nil) {
+        self.viewModel = viewModel
+        let layout = UICollectionViewFlowLayout()
+        super.init(frame: .zero, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HomeCollectionView: HomeCollectionViewInterface {
     
     
 }
--- LBC/Views/Home/HomeViewController.swift
+++ LBC/Views/Home/HomeViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    lazy var collectionView: HomeCollectionView = {
+        let cv = HomeCollectionView()
+        cv.viewModel = viewModel
+        return cv
+    }()
+    
+    let viewModel: HomeViewModel
+    
+    // MARK: - Life Cycle
+    
+    init() {
+        self.viewModel = HomeViewModel()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupBindings()
     }
-
-
+    
+    // MARK: - Setup Views
+    
+    private func setupBindings() {
+        
+        // Make sure references are weak.
+        viewModel.collectionView = collectionView
+        collectionView.viewModel = viewModel
+    }
+    
 }
 
