186162815e94d67cf616bdd1a235057940030b13
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-30 10:03:37 +0200

    Adds a MockNetworkService and the firsts tests for HomeViewModel

--- LBCTests/LBCTests.swift
+++ LBCTests/LBCTests.swift
@@ -6,31 +6,57 @@
 //
 
 import XCTest
+import Combine
 @testable import LBC
 
 final class LBCTests: XCTestCase {
+    
+    var viewModel: HomeViewModel!
+    var mockNetworkService: MockNetworkService!
+    var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        mockNetworkService = MockNetworkService()
+        viewModel = HomeViewModel(networkService: mockNetworkService)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        mockNetworkService = nil
+        viewModel = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFetchDataSuccess() {
+        
+        let categoriesData = loadMock(name: "categories", ext: "json")
+        let productsData = loadMock(name: "products", ext: "json")
+        
+        mockNetworkService.categoriesPublisher = Just(categoriesData)
+            .setFailureType(to: NetworkServiceError.self)
+            .eraseToAnyPublisher()
+        
+        mockNetworkService.productsPublisher = Just(productsData)
+            .setFailureType(to: NetworkServiceError.self)
+            .eraseToAnyPublisher()
+        
+        let expectation = XCTestExpectation(description: "Successfully fetched data")
+        
+        viewModel.$products
+            .dropFirst()
+            .sink { products in
+                print("products.count, ",products.count)
+                XCTAssertEqual(products[0].category, "Maison")
+                XCTAssertEqual(products[1].category, "Multimédia")
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        viewModel.start()
+        
+        wait(for: [expectation], timeout: 5.0)
     }
 
 }
